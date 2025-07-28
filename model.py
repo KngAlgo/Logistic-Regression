18427f46653feeb6355426a47d54ea1d8b8c2011
KngAlgo <chamboscott@gmail.com>
2025-07-27 21:49:41 -0500

    Created Predictions (0.92 acc)

--- model.py
+++ model.py
@@ -1,5 +1,8 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+
+import matplotlib.pyplot as plt
 
 import pandas as pd
 import numpy as np
@@ -8,15 +11,16 @@ import numpy as np
 
 train_data = pd.read_csv('train.csv')
 test_data = pd.read_csv('test.csv')
-y_true = pd.read_csv('gender_submission.csv')
+true_data = pd.read_csv('gender_submission.csv')
 
-x_train = train_data.drop(columns=['Survived', 'Name', 'Ticket', 'Cabin'], axis=1)
+y_true = true_data['Survived']
+
+x_train = train_data.drop(columns=['Survived', 'Name', 'Ticket', 'Cabin', 'PassengerId'], axis=1)
 y_train = train_data['Survived']
 
-x_test = test_data.drop(columns=['Name', 'Ticket', 'Cabin'], axis=1)
+x_test = test_data.drop(columns=['Name', 'Ticket', 'Cabin', 'PassengerId'], axis=1)
 
 # Scaling
-
 
 x_train['Age'].fillna(x_train['Age'].mean(), inplace=True)
 x_train['Fare'].fillna(x_train['Fare'].mean(), inplace=True)
@@ -35,18 +39,23 @@ x_test['Sex'] = x_test['Sex'].map({'male': 0, 'female': 1})
 
 # Model Instantiation and Fitting
 
-print(x_train.head())
-
-print(x_test.head())
-# Show columns with missing values in train_data
-print("Missing values in train_data:")
-print(x_train.isnull().sum())
-
-# Show columns with missing values in test_data
-print("\nMissing values in test_data:")
-print(x_test.isnull().sum())
-
 model = LogisticRegression(max_iter=200)
 
 model.fit(x_train, y_train)
 
+# Create Predictions
+
+y_pred = model.predict(x_test)
+
+plt.figure(figsize=(6,4))
+plt.bar(['Not Survived', 'Survived'], [sum(y_pred == 0), sum(y_pred == 1)], color=['red', 'green'])
+plt.title('Predicted Survival Counts')
+plt.ylabel('Number of Passengers')
+plt.xlabel('Survival Prediction')
+plt.show()
+
+# 253 TN, 13 FP, 12 FN, 140 TP
+print(confusion_matrix(y_true, y_pred))
+
+accuracy = (418-25)/425 #0.9247
+print(accuracy)
